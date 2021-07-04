00344757593c438bec8fa2306451ddfe08fbc4fd
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-07-04 09:23:43 +0100

    lessons

---
+++ app.js
@@ -0,0 +1,43 @@
+function b(){
+    var myVar 
+    console.log(myVar)
+}
+
+function a(){
+    var myVar = 2
+    console.log(myVar)
+    b()
+}
+
+var myVar = 1
+console.log(myVar)
+a()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a;
+
+// a = undefined
+
+// if(a === undefined){
+//     console.log("i am not defined")
+
+// }else{
+//     console.log(a)
+//     console.log("i am defined")
+// }
+
